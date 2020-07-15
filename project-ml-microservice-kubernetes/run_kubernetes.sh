8001fa4b4d49b762ad4b6ce0a458422c025c8aba
Kirsten Cassidy <mantismamita@gmail.com>
2020-07-15 20:19:49 +0200

    begins circle-ci setup and adds sh scripts files

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,20 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="mantismamita/microservices-proj"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run microservices-proj\
+  --generator=run-pod/v1\
+  --image=$dockerpath\
+  --port=80 --labels app=microservices-proj
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward microservices-proj 8000:80
 
